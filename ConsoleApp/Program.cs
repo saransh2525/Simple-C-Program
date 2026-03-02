cb2a54e8c9e66e097a3a0af6617b3685592c0e41
Saransh Rathi <gurjaraashish025@gmail.com>
2026-03-02 10:48:56 +0530

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Abc
+{
+    public void print(){
+        Console.WriteLine("hello print1");
+            }
+    public void print2()
+    {
+        Console.WriteLine("hello print2");
+            }
+
+}
+class Xyz
+{
+    public static void Main(String[] args)
+    {
+        Abc obj = new Abc();
+        obj.print();
+        obj.print2();
+    }
+}
