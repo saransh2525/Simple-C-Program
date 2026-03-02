34cd6ff91aa0e432b1f88fbd8800ad9125c68fa1
Saransh Rathi <gurjaraashish025@gmail.com>
2026-03-02 11:40:26 +0530

    new line added

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,25 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-class Abc
-{
-    public void print(){
-        Console.WriteLine("hello print1");
-            }
-    public void print2()
-    {
-        Console.WriteLine("hello print2");
-            }
-    public void print3()
-    {
-        Console.WriteLine("hello print3");
-            }
-}
-class Xyz
-{
-    public static void Main(String[] args)
-    {
-        Abc obj = new Abc();
-        obj.print();
-        obj.print2();
-        obj.print3();
-    }
-}
+Console.WriteLine("hello");
+Console.WriteLine("hello");
